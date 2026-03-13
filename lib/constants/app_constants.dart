2c78da03ae33e9d0fd96d5853f104f2226dd23a8
PYJUU <1375906627@qq.com>
2026-03-14 01:24:08 +0800

    修复

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,22 @@
+import 'package:thingsboard_app/modules/main/model/navigation_type.dart';
+
+abstract final class ThingsboardAppConstants {
+  static const thingsBoardApiEndpoint = String.fromEnvironment(
+    'thingsboardApiEndpoint',
+    defaultValue: 'http://thingsboard.newinte.top:8080',
+  );
+  static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment(
+    'thingsboardOAuth2CallbackUrlScheme',
+  );
+  static const thingsboardIOSAppSecret = String.fromEnvironment(
+    'thingsboardIosAppSecret',
+  );
+  static const thingsboardAndroidAppSecret = String.fromEnvironment(
+    'thingsboardAndroidAppSecret',
+  );
+  static const ignoreRegionSelection = thingsBoardApiEndpoint != '';
+  static final navigationType = 
+  TbNavigationType.fromString(
+  const String.fromEnvironment('navigationType'),
+  );
+}
---
+++ lib/modules/main/providers/navigation_helper.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:thingsboard_app/generated/l10n.dart';
+import 'package:thingsboard_app/modules/main/model/navigation_item_data.dart';
+import 'package:thingsboard_app/modules/notification/widgets/notification_icon.dart';
+import 'package:thingsboard_app/thingsboard_client.dart';
+
+class NavigationHelper {
+static  int? getCurrentIndexFromPath(String path, List<NavigationItemData> items) {
+ 
+    for (int i = 0; i < items.length; i++) {
+      if (items[i].path == path) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+// Helper functions
+static String getLabel(PageLayout pageLayout) {
+  if (pageLayout.label != null) {
+    return pageLayout.label!;
+  }
+
+  switch (pageLayout.id) {
+    case Pages.home:
+      return S.current.home;
+    case Pages.alarms:
+      return S.current.alarms(2);
+    case Pages.devices:
+      return S.current.devices(2);
+    case Pages.customers:
+      return S.current.customers;
+    case Pages.assets:
+      return S.current.assets;
+    case Pages.audit_logs:
+      return S.current.auditLogs;
+    case Pages.notifications:
+      return S.current.notifications(2);
+    case Pages.device_list:
+      return S.current.allDevices;
+    case Pages.dashboards:
+      return S.current.dashboards(2);
+    case Pages.undefined:
+    case null:
+      return pageLayout.label ?? '-';
+  }
+}
+
+static IconData getIcon(PageLayout pageLayout) {
+  if (pageLayout.icon != null) {
+    return getIconFromString(pageLayout.icon);
+  }
+
+  switch (pageLayout.id) {
+    case Pages.home:
+      return Icons.home_outlined;
+    case Pages.alarms:
+      return Icons.notifications_outlined;
+    case Pages.devices:
+      return Icons.devices_other_outlined;
+    case Pages.customers:
+      return Icons.supervisor_account_sharp;
+    case Pages.assets:
+      return Icons.domain_outlined;
+    case Pages.audit_logs:
+      return Icons.track_changes_outlined;
+    case Pages.notifications:
+      return Icons.notifications_active_sharp;
+    case Pages.device_list:
+      return Icons.devices;
+    case Pages.dashboards:
+      return Icons.dashboard_outlined;
+    case Pages.undefined:
+    case null:
+      return getIconFromString(pageLayout.icon);
+  }
+}
+
+static  String getPath(PageLayout pageLayout) {
+  switch (pageLayout.id) {
+    case Pages.home:
+      return '/home';
+    case Pages.alarms:
+      return '/alarms';
+    case Pages.devices:
+      return '/devices';
+    case Pages.customers:
+      return '/customers';
+    case Pages.assets:
+      return '/assets';
+    case Pages.audit_logs:
+      return '/auditLogs';
+    case Pages.notifications:
+      return '/notifications';
+    case Pages.device_list:
+      return '/deviceList';
+    case Pages.dashboards:
+      return '/dashboards';
+    case Pages.undefined:
+    case null:
+      if (pageLayout.url != null) {
+        return '/url/${Uri.encodeComponent(pageLayout.url!)}';
+      } else if (pageLayout.dashboardId != null) {
+        return '/dashboard/${pageLayout.dashboardId}';
+      } else if (pageLayout.path?.startsWith('/url/') == true) {
+        return '/url/${Uri.encodeComponent(pageLayout.path!.split('/url/').last)}';
+      }
+
+      return pageLayout.path ?? '/error';
+  }
+}
+
+static  IconData getIconFromString(String? icon) {
+  if (icon != null) {
+    if (icon.contains('mdi')) {
+      return MdiIcons.fromString(icon.split('mdi:').last) ??
+          Icons.error_outline;
+    }
+
+    return materialIconsMap[icon] ?? Icons.error_outline;
+  }
+  return Icons.error_outline;
+}
+
+}
